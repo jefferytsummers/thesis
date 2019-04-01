d9f5ad59d3a888b6e5ad590e8597a5a5dc3b0b3c
Jeffery <jefferytsummers@gmail.com>
2019-04-01 17:48:35 -0500

    Initial commit.

---
+++ figures/plotdata.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import csv
+import numpy as np
+
+xdata = []
+ydata = []
+data = [[],[]]
+filename = '20-55-25.csv'
+infile = open(filename, 'r')
+
+count = 0
+for line in infile:
+    num= ''
+    for element in line:
+        if element != ',':
+            num += element
+        else:
+            data[count].append(int(num))
+            num = ''
+            continue
+    
+    count += 1
+infile.close()
+data[1] = [_/10 for _ in data[1]]
+data[0] = [_ if _ != 0 else np.nan for _ in data[0]]
+
+plt.plot(data[1], data[0])
+plt.ylabel('Mode position (px)')
+plt.xlabel('Time (s)')
+plt.show()
