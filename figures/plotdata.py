e97dadc4e09424fd8ceb2a8ecbfe42daa196a441
Jeffery <jefferytsummers@gmail.com>
2019-04-12 17:37:10 -0500

    Added abstract

--- figures/plotdata.py
+++ figures/plotdata.py
@@ -5,7 +5,7 @@ import numpy as np
 xdata = []
 ydata = []
 data = [[],[]]
-filename = '20-55-25.csv'
+filename = '22-21-50.csv'
 infile = open(filename, 'r')
 
 count = 0
@@ -22,7 +22,7 @@ for line in infile:
     count += 1
 infile.close()
 data[1] = [_/10 for _ in data[1]]
-data[0] = [_ if _ != 0 else np.nan for _ in data[0]]
+data[0] = [_ if _ > 450 else np.nan for _ in data[0]]
 
 plt.plot(data[1], data[0])
 plt.ylabel('Mode position (px)')
